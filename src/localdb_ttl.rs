3e36075201c2397bdb6ab1f62cf855daa8df8d6d
Liam <dltmdgud@nate.com>
2025-12-27 14:05:05 +0900

    expired된 키들을 삭제이후에 똑같은 값이 들어올때 인덱스가 사라지는 이슈 또한 비정상종료일 때 access db 값이 무제한으로 남을 수 있는 문제 해결.

--- src/localdb_ttl.rs
+++ src/localdb_ttl.rs
@@ -2,9 +2,10 @@ use chrono::{DateTime, Utc};
 use keyed_lock::r#async::KeyedLock;
 use rocksdb::{
     BlockBasedOptions, Cache, DBCompressionType, DBWithThreadMode, IteratorMode, MultiThreaded,
-    Options, ReadOptions, WriteOptions,
+    Options, ReadOptions, WriteBatch, WriteOptions,
 };
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::{path::Path, sync::Arc};
 use tokio::task::{self, JoinHandle};
 
@@ -195,6 +196,12 @@ impl TTLIndexInner {
             .map_err(anyhow::Error::from)
     }
 
+    fn commit(&self, batch: WriteBatch) -> anyhow::Result<()> {
+        self.db
+            .write_opt(batch, &self.write_opts)
+            .map_err(anyhow::Error::from)
+    }
+
     /// 만료된 항목들을 스캔합니다.
     ///
     /// RocksDB의 정렬 기능을 이용하여 앞에서부터 순차적으로 만료된 항목들을 스캔합니다.
@@ -205,9 +212,10 @@ impl TTLIndexInner {
     ///
     /// # Returns
     /// * `Vec<(i64, Vec<u8>)>` - 만료된 항목들의 리스트 (timestamp, AccessDB key)
-    fn scan_expired(&self, now: DateTime<Utc>) -> anyhow::Result<Vec<(Vec<u8>, Vec<u8>)>> {
+    fn scan_expired(&self, now: DateTime<Utc>) -> anyhow::Result<HashMap<Vec<u8>, Vec<u8>>> {
         let now = now.timestamp_millis();
-        let mut results = Vec::new();
+
+        let mut results = HashMap::new();
 
         // Iterator를 사용하여 앞에서부터 순차적으로 만료된 항목들을 스캔
         // timestamp가 정렬되어 있으므로 now_timestamp 이하인 항목들만 수집
@@ -220,7 +228,7 @@ impl TTLIndexInner {
             // now_timestamp 이하인 경우에만 추가
             if timestamp <= now {
                 let key = Self::db_key_to_key(&db_key);
-                results.push((db_key.into_vec(), key));
+                results.insert(db_key.into_vec(), key);
             } else {
                 // timestamp가 정렬되어 있으므로 이후 항목은 모두 만료되지 않음
                 break;
@@ -237,17 +245,16 @@ impl TTLIndexInner {
     ///
     /// # Returns
     /// * `usize` - 삭제된 항목의 개수
-    fn delete_expired(&self, expired: Vec<(Vec<u8>, Vec<u8>)>) -> anyhow::Result<()> {
+    fn delete_expired(&self, expired: HashMap<Vec<u8>, Vec<u8>>) -> anyhow::Result<()> {
         // 삭제할 항목들을 WriteBatch로 일괄 삭제
-        let mut batch = rocksdb::WriteBatch::default();
-        for (db_key, _key) in &expired {
-            batch.delete(&db_key);
+        let mut batch = WriteBatch::default();
+
+        for db_key in expired.keys() {
+            batch.delete(db_key);
         }
 
         if expired.len() > 0 {
-            self.db
-                .write_opt(batch, &self.write_opts)
-                .map_err(anyhow::Error::from)?;
+            self.commit(batch)?;
         }
 
         Ok(())
@@ -317,6 +324,11 @@ impl TTLIndex {
         let inner = self.inner.clone();
         let key = key.to_vec();
         task::spawn_blocking(move || inner.insert(expire_at, &key)).await?
+    }
+
+    pub async fn commit(&self, batch: WriteBatch) -> anyhow::Result<()> {
+        let inner = self.inner.clone();
+        task::spawn_blocking(move || inner.commit(batch)).await?
     }
 
     /// TTL 인덱스에서 항목이 존재하는지 확인합니다.
@@ -408,7 +420,7 @@ impl TTLIndex {
     pub async fn scan_expired(
         &self,
         now: DateTime<Utc>,
-    ) -> anyhow::Result<Vec<(Vec<u8>, Vec<u8>)>> {
+    ) -> anyhow::Result<HashMap<Vec<u8>, Vec<u8>>> {
         let inner = self.inner.clone();
         task::spawn_blocking(move || inner.scan_expired(now)).await?
     }
@@ -435,7 +447,7 @@ impl TTLIndex {
     /// # Ok(())
     /// # }
     /// ```
-    pub async fn delete_expired(&self, expired: Vec<(Vec<u8>, Vec<u8>)>) -> anyhow::Result<()> {
+    pub async fn delete_expired(&self, expired: HashMap<Vec<u8>, Vec<u8>>) -> anyhow::Result<()> {
         let inner = self.inner.clone();
         task::spawn_blocking(move || inner.delete_expired(expired)).await?
     }
@@ -559,12 +571,19 @@ impl LocalDBTTL {
 
     async fn delete_expired(ctx: Arc<Context>) -> anyhow::Result<()> {
         let now = Utc::now();
-        let expired = ctx.ttl_index.scan_expired(now).await?;
-        for (index, key) in &expired {
+
+        let mut expired = ctx.ttl_index.scan_expired(now).await?;
+        let mut deleted_keys = Vec::new();
+        for (index, key) in expired.iter() {
             let _guard = ctx.locks.lock(key.clone()).await;
             if ctx.ttl_index.includes(&index).await? {
                 ctx.access_db.delete(key.clone()).await?;
+            } else {
+                deleted_keys.push(index.clone());
             }
+        }
+        for db_key in deleted_keys {
+            expired.remove(&db_key);
         }
         ctx.ttl_index.delete_expired(expired).await?;
         Ok(())
@@ -593,23 +612,28 @@ impl LocalDBTTL {
         };
         let serialized = postcard::to_stdvec(&new_item)?;
 
+        let mut batch = WriteBatch::default();
         let _guard = self.ctx.locks.lock(key.clone()).await;
+
         if let Some(serialized) = self.ctx.access_db.get(key.clone()).await? {
-            let db_value: DBSchema = postcard::from_bytes(&serialized)?;
-            self.ctx
-                .ttl_index
-                .remove(db_value.expire_at(), &key)
-                .await?;
+            let old_item: DBSchema = postcard::from_bytes(&serialized)?;
+            if old_item.expire_at() != new_item.expire_at() {
+                batch.delete(TTLIndexInner::timestamp_and_key_to_db_key(
+                    &old_item.expire_at(),
+                    &key,
+                ));
+                batch.put(
+                    TTLIndexInner::timestamp_and_key_to_db_key(&new_item.expire_at(), &key),
+                    &[],
+                );
+                self.ctx.ttl_index.commit(batch).await?;
+            }
+        } else {
+            self.ctx.ttl_index.insert(new_item.expire_at(), &key).await?;
         }
 
         // AccessDB에 저장
-        self.ctx.access_db.put(key.clone(), serialized).await?;
-
-        // TTL 인덱스에 추가
-        self.ctx
-            .ttl_index
-            .insert(new_item.expire_at(), &key)
-            .await?;
+        self.ctx.access_db.put(key, serialized).await?;
 
         Ok(())
     }
@@ -623,7 +647,7 @@ impl LocalDBTTL {
     ///
     /// # Returns
     /// * `Option<(AccessDBValue, bool)>` - (값, updated 여부)
-    pub async fn get(&self, key: Vec<u8>) -> anyhow::Result<Option<Vec<u8>>> {
+    pub async fn get(&self, key: &Vec<u8>) -> anyhow::Result<Option<Vec<u8>>> {
         let _guard = self.ctx.locks.lock(key.clone()).await;
         // AccessDB에서 조회
         let serialized = match self.ctx.access_db.get(key.clone()).await? {
@@ -637,29 +661,33 @@ impl LocalDBTTL {
 
         let now = Utc::now();
         if now > db_value.expire_at() {
-            self.ctx
-                .ttl_index
-                .remove(db_value.expire_at(), &key)
-                .await?;
-            self.ctx.access_db.delete(key).await?;
+            self.ctx.access_db.delete(key.clone()).await?;
+
+            self.ctx.ttl_index.remove(db_value.expire_at(), key).await?;
             return Ok(None);
         }
 
         // idle 상태이고 아직 만료되지 않았으면 updated_at 업데이트
         match &db_value.expired_at {
             ExpiredAt::Idle(_) => {
-                self.ctx
-                    .ttl_index
-                    .remove(db_value.expire_at(), &key)
-                    .await?;
+                let previous_expire_at = db_value.expire_at();
 
                 db_value.updated_at = now;
                 let serialized = postcard::to_stdvec(&db_value)?;
-                self.ctx
-                    .ttl_index
-                    .insert(db_value.expire_at(), &key)
-                    .await?;
-                self.ctx.access_db.put(key, serialized).await?;
+                self.ctx.access_db.put(key.clone(), serialized).await?;
+                if previous_expire_at != db_value.expire_at() {
+                    let mut batch = WriteBatch::default();
+                    batch.delete(TTLIndexInner::timestamp_and_key_to_db_key(
+                        &previous_expire_at,
+                        key,
+                    ));
+                    batch.put(
+                        TTLIndexInner::timestamp_and_key_to_db_key(&db_value.expire_at(), key),
+                        &[],
+                    );
+                    self.ctx.ttl_index.commit(batch).await?;
+                }
+
                 Ok(Some(db_value.value))
             }
             ExpiredAt::Live(_) => Ok(Some(db_value.value)),
@@ -670,17 +698,15 @@ impl LocalDBTTL {
     ///
     /// # Arguments
     /// * `key` - 키 (Vec<u8>)
-    pub async fn delete(&self, key: Vec<u8>) -> anyhow::Result<()> {
+    pub async fn delete(&self, key: &Vec<u8>) -> anyhow::Result<()> {
         let _guard = self.ctx.locks.lock(key.clone()).await;
         // AccessDB에서 조회하여 expired_at 확인
         if let Some(serialized) = self.ctx.access_db.get(key.clone()).await? {
             let db_value: DBSchema = postcard::from_bytes(&serialized)?;
+            self.ctx.access_db.delete(key.clone()).await?;
+
             // TTL 인덱스에서 제거
-            self.ctx
-                .ttl_index
-                .remove(db_value.expire_at(), &key)
-                .await?;
-            self.ctx.access_db.delete(key).await?;
+            self.ctx.ttl_index.remove(db_value.expire_at(), key).await?;
         }
 
         Ok(())
